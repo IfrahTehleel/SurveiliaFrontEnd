183beb0b15b78da1d0e255d8e1766678a5813669
IfrahTehleel <55829522+IfrahTehleel@users.noreply.github.com>
2020-09-25 18:00:10 +0500

    frontend code

--- database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-def createTable():
-    connection = sqlite3.connect("login.db")
-    connection.execute(
-        "CREATE TABLE USERS ( USER_ID INTEGER PRIMARY KEY, USERNAME TEXT NOT NULL, PASSWORD NOT NULL, CONTACT_INFO TEXT NOT NULL UNIQUE, ADDRESS TEXT ")
-
-    connection.execute("")
